918a6f6d0a13c82782571139bfc17c8b46492701
Keaton Law <keaton.c.law@gmail.com>
2022-08-23 19:38:54 -0500

    mergedData

---
+++ pages/Recipe/[firebaseKey].js
@@ -0,0 +1,29 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import viewRecipeDetails from '../../API/mergedData';
+
+export default function ViewRecipeDetail() {
+  const [recipeDetail, setRecipeDetail] = useState({});
+  const router = useRouter();
+  const { firebaseKey } = router.query;
+
+  useEffect(() => {
+    viewRecipeDetails(firebaseKey).then(setRecipeDetail);
+  }, [firebaseKey]);
+  return (
+    <div className="mt-5 d-flex flex-wrap">
+      <div className="d-flex flex-column">
+        <img src={recipeDetail.image} alt={recipeDetail.name} style={{ width: '300px' }} />
+      </div>
+      <div className="text-white ms-5 details">
+        <h5>
+          {recipeDetail.name}
+        </h5>
+        <p>
+          {recipeDetail.directions}
+        </p>
+      </div>
+    </div>
+  );
+}
